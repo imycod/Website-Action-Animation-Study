a1391bd9fe1fb4ec55803af04451e6ddd92c121b
wuxs <317009160@qq.com>
2023-09-03 00:51:53 +0800

    feat

--- responsive-web-design/index.js
+++ responsive-web-design/index.js
@@ -1,4 +1,25 @@
-window.addEventListener('scroll', function() {
+// header头滚动吸顶
+window.addEventListener('scroll', function () {
     var header = document.querySelector('header');
-    header.classList.toggle('sticky', window.scrollY > 0);
+    header.classList.toggle('sticky', window.scrollY > 10);
 });
+// 锚点点击后的过渡效果js版
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// 锚点点击后的过渡效果jq版
+// $('a[href^="#"]').on('click', function(event) {
+//     var target = $(this.getAttribute('href'));
+//     if(target.length) {
+//         event.preventDefault();
+//         $('html, body').stop().animate({
+//             scrollTop: target.offset().top
+//         }, 1000);
+//     }
+// });
